86e0def8e6a2c831536ea0fcf7b8685a708bd82c
Maria Eduarda Benício <69880695+mbenicio@users.noreply.github.com>
2021-04-25 19:18:42 -0300

    Consultar Cep

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Cliente } from './cliente';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'consulta-cep';
+  cliente: Cliente = new Cliente();
+  
+  constructor(private http: HttpClient) { }
+
+  consultaCEP() {
+    this.http.get(`https://viacep.com.br/ws/${this.cliente.CEP}/json/`).toPromise().then(
+      data => {
+        const dado: any = data;
+        this.cliente.Endereco = dado.logradouro;
+      }
+    );
+  }
+  salvar() {
+    console.log(this.cliente);
+  }
+}
